27db1cab32301a49f186cbc0db61cb7993a85ce6
Kananelo Maxwell <kananelom.maxwell4@gmail.com>
2019-02-05 13:10:00 +0200

    Update Hello.java
    Refactoring code. Making it cleaner.

--- 001/Hello.java
+++ 001/Hello.java
@@ -1,12 +1,12 @@
 public class Hello {
-public static void main(String[] args) {        
-        if (args.length == 0) {
-            System.out.println("Here is a list of arguments.");
+    public static void main(String[] args) {        
+        if (args.length > 0) {
+            // Loop through all the elements of the array.
             for (int x = 0; x < args.length; x++) {
-                System.out.println(args[x]);
+                System.out.println(" - " + args[x]);
             }
         } else {
-            System.out.println("There are not arguments passed!");
+            System.out.println("No arguments passed!");
         }
-  } // end static method
+    } // end static method
 } // end class
