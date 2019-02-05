68d417a7bddc544d59b6411b5af44807dd5a1eec
Kananelo Maxwell <kananelom.maxwell4@gmail.com>
2019-02-05 16:03:02 +0200

    Add files via upload

--- Practica01/build.bat
+++ Practica01/build.bat
@@ -1,3 +1,23 @@
-echo off
-javac -d ..\bin Main.java
-java Main
+@echo off
+
+REM Author: Kananelo Maxwell
+REM Student#: 217080976
+REM Practical: 00
+
+cd ..
+
+set %PATH%  = %PATH%;C:\Program Files\Java\jdk1.8.0_201\bin
+
+REM echo %PATH%
+
+set BIN=.\bin
+set SRC=.\acsse.csc2a
+set DOC=.\docs
+
+javac -sourcepath %SRC% -d %BIN% %SRC%\Main.java
+java -cp %BIN% Main
+javap -c %BIN%\Main.class >> %DOC%\ByteCode.txt
+
+REM move ByteCode.txt %DOC%
+
+cd %DOC%
