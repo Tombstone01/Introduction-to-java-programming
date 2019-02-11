66522317e68d5c534dccc43ffaa7f83480e5aafe
Kananelo Maxwell <kananelom.maxwell4@gmail.com>
2019-02-11 09:53:08 +0200

    Add files via upload

---
+++ Practical2/Main.java
@@ -0,0 +1,15 @@
+
+
+class Main {
+	public static void main(String[] args) {
+
+		String name;
+		String resuls;
+		String encode;
+
+		/** Creates an instance of class NameConverter.
+		 **/
+		NameConverter converter = new NameConverter();
+
+	}
+}
