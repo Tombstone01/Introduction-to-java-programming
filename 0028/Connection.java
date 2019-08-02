250ef107f036faa99461547fd14abd04dbc610d2
Kananelo <Kananelom.maxwell4@gmail.com>
2019-08-02 08:36:51 +0200

    Finished working on practical 1.

--- 0028/Connection.java
+++ 0028/Connection.java
@@ -3,65 +3,91 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Random;
 
-/** @author Kananelo Maxwell
+/**
+ * @author Kananelo Maxwell
  */
 
 public class Connection implements Runnable {
 
-	private Socket socket;
-	private PrintWriter printWriter;
-	private BufferedReader bufferedReader;
+  private Socket socket;
+  private PrintWriter printWriter;
+  private BufferedReader bufferedReader;
 
   public Connection(Socket socket) {
-		this.socket = socket;
+    this.socket = socket;
 
-		try {
-			bufferedReader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-			printWriter = new PrintWriter(this.socket.getOutputStream());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    try {
+      bufferedReader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+      printWriter = new PrintWriter(this.socket.getOutputStream(), true);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
-	public void run() {
+  public void run() {
 
-		boolean done = false;
-		int counter = 0;
-		String response = "";
+    boolean done = false;
+    int counter = 0;
+    String response = "";
 
-		printWriter.println("What question would you like to ask?");
-		printWriter.flush();
-  
+    printWriter.println("What question would you like to ask?");
+
     while (!done) {
-			try {
-				// read response from the client.
-				response = bufferedReader.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+      try {
+        // read response from the client.
+        response = bufferedReader.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
       // if response starts with "ASK"
       if (response.startsWith("ASK")) {
-				// send response back to the user.
+
+        // split the response into words.
+        String[] question = response.split(" ");
+
+        // if the question starts with why?
+        if (question[1].equalsIgnoreCase("why")) {
+          printWriter.println("Because that is how it is.");
+        } else if (question[1].equalsIgnoreCase("Are")) {
+          String[] clientResponse = { "Yes", "No", "Maybe" };
+
+          // randomly select a number between 0 and 3;
+          double index = Math.floor((Math.random() * 3));
+
+          // pick a random response to the client.
+          printWriter.println(clientResponse[(int) index]);
+        } else {
+
+          String[] clientResponse = { "It depends", "Please ask again later", "Meh" };
+
+          // randomly select a number between 0 and 3;
+          double index = Math.floor((Math.random() * 3));
+
+          // pick a random response to the client.
+          printWriter.println(clientResponse[(int) index]);
+        }
+
+        // send response back to the user.
         printWriter.println(counter + " response here.");
-        printWriter.flush();
       }
-	
-			/** if the number of question exceeds 5 or if the client is 
-			 *  done asking question.
-			 */
+
+      /**
+       * if the number of question exceeds 5 or if the client is done asking question.
+       */
       if (counter == 5 || response.equalsIgnoreCase("done")) {
 
-				// terminate the loop.
+        // terminate the loop.
         done = true;
-	
-				// send the connection closed message.
+
+        // send the connection closed message.
         printWriter.println("05 HAVE A NICE DAY - 5 Questions answered");
         printWriter.flush();
-			}
-			
-			// increment the number of questions asked.
-			counter++;
+      }
+
+      // increment the number of questions asked.
+      counter++;
     }
-	}
+  }
 }
--- 0028/Main.java
+++ 0028/Main.java
@@ -5,35 +5,38 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * @author Kananelo Maxwell
+ */
+
 public class Main {
+
+  private static int PORT_NUM = 8888;
+
   public static void main(String[] args) {
 
     ServerSocket server = null;
     Socket clientSocket = null;
-    PrintWriter printWriter = null;
-    BufferedReader bufferedReader = null;
-
-    int PORT_NUM = 8888;
 
     try {
 
       // create a server on port 8080
-      server = new ServerSocket(8080);
+      server = new ServerSocket(PORT_NUM);
 
       // accept incoming server connection.
       clientSocket = server.accept();
 
-      Thread t1 = new Thread(new Connection(clientSocket));
-      t1.start();
+      // create a thread.
+      Thread thread = new Thread(new Connection(clientSocket));
 
-      // // Tell the client that the can go ahead and ask some questions.
-      // printWriter.println("01 WELCOME - You may ask 5 questions");
-      // printWriter.flush();
+      // start the thread.
+      thread.start();
 
     } catch (IOException e) {
       e.printStackTrace();
     } finally {
       try {
+        // close the server here.
         server.close();
       } catch (IOException e) {
         e.printStackTrace();
