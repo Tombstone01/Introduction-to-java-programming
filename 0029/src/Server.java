5fe53eaf1f0ed2f303ec23cbbaeee6ca6c92dad6
Kananelo <Kananelom.maxwell4@gmail.com>
2019-08-13 14:02:03 +0200

    Finished working on the practical.

--- 0029/src/Server.java
+++ 0029/src/Server.java
@@ -3,21 +3,26 @@ import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 
 public class Server implements Runnable {
 
   private BufferedReader in;
 
+  private PrintWriter printWriter;
+
   // Writing binary data, like images and video.
-  private DataOutputStream out;
+  private BufferedOutputStream out;
 
   public Server(Socket socket) throws IOException {
 
     in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-    out = new DataOutputStream(socket.getOutputStream());
+    printWriter = new PrintWriter(socket.getOutputStream());
+    out = new BufferedOutputStream(socket.getOutputStream());
   }
 
   public void run() {
@@ -30,49 +35,58 @@ public class Server implements Runnable {
 
       if (request.equalsIgnoreCase("/") || request.contains("index")) {
 
-        File file = new File("../public/index.html");
+        File[] files = new File[2];
+        files[0] = new File("../public/index.html");
+        files[1] = new File("../public/small.mp4");
 
-        // send header.
-        out.writeBytes("HTTP/1.1 200 OK\r\n");
-        out.writeBytes("Content-Type: text/html\r\n");
-        out.writeBytes("Content-Length: " + file.length());
-        out.writeBytes("Connection: close\r\n");
-        out.writeBytes("\r\n");
+        BufferedInputStream bin = null;
 
-        // open stream.
-        BufferedInputStream bin = new BufferedInputStream(new FileInputStream(file));
-        byte[] buffer = new byte[4096];
+        for (int x = 0; x < files.length; x++) {
 
-        int n = 0;
+          // send header.
+          printWriter.println("HTTP/1.1 200 OK\r\n");
 
-        // while there is still content.
-        while ((n = bin.read(buffer)) > 0) {
-          out.write(buffer, 0, n);
+          System.out.println(files[x].toString().split("."));
+
+          if (x == 0) {
+            printWriter.println("Content-Type: text/html\r\n");
+          } else {
+            printWriter.println("Content-Type: video/mp4\r\n");
+          }
+          printWriter.println("Content-Length: " + files[x] + "\r\n");
+          printWriter.println("Connection: close\r\n");
+          printWriter.println("\r\n");
+
+          // open stream.
+          bin = new BufferedInputStream(new FileInputStream(files[x]));
+          byte[] buffer = new byte[4096];
+
+          int n = 0;
+
+          // while there is still content.
+          while ((n = bin.read(buffer)) > 0) {
+            out.write(buffer, 0, n);
+          }
         }
 
         bin.close();
 
       } else {
         // send header.
-        out.writeBytes("HTTP/1.1 404 OK\r\n");
-        out.writeBytes("Content-Type: text/plain\r\n");
-        out.writeBytes("Connection: close\r\n");
-        out.writeBytes("\r\n");
+        printWriter.println("HTTP/1.1 404 OK\r\n");
+        printWriter.println("Content-Type: text/plain\r\n");
+        printWriter.println("Connection: close\r\n");
+        printWriter.println("\r\n");
 
-        out.writeBytes("404 Not Found");
+        // out.("404 Not Found");
 
       }
     } catch (IOException e) {
       e.printStackTrace();
 
-      try {
-        // send header.
-        out.writeBytes("HTTP/1.1 500 OK\r\n");
-        out.writeBytes("Connection: close\r\n");
-        out.writeBytes("\r\n");
-      } catch (IOException y) {
-        y.printStackTrace();
-      }
+      printWriter.println("HTTP/1.1 500 OK\r\n");
+      printWriter.println("Connection: close\r\n");
+      printWriter.println("\r\n");
     } catch (NullPointerException e) {
       e.printStackTrace();
     }
