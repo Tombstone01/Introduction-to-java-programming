f7221e804b97c9b96b9ea015fc270c655922bfd0
Kananelo <Kananelom.maxwell4@gmail.com>
2019-08-20 14:30:22 +0200

    Java katas are hard.

---
+++ 0032/Kata.java
@@ -0,0 +1,18 @@
+// Make sure your class is public
+public class Kata {
+
+	public static void main(String[] args) {
+		System.out.println(findUniq(new double[] { 1, 1, 1, 2, 1, 1 }));
+	}
+
+	public static double findUniq(double arr[]) {
+
+		double one = arr[0];
+
+		for (int x = 1; x < arr.length; x++) {
+
+		}
+
+		return f;
+	}
+}
