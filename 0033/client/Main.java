b4ac097ac16a90a65e800bce2aa8bf28eb032b1d
Kananelo Maxwell <kananelom.maxwell4@gmail.com>
2019-09-07 16:33:33 +0200

    Started working on CSC 2B practical assignment 5.

---
+++ 0033/client/Main.java
@@ -0,0 +1,56 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.Scanner;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+
+public class Main {
+
+	// this is the UPD port that the server runs on.
+	private static int UDP_PORT = 9876;
+
+  public static void main(String[] args) throws Exception {
+	
+		// this socket is used to communicated with the server.
+		DatagramSocket clientSocket = new DatagramSocket();
+
+		System.out.println("Server running");
+
+		// while the user is not done.
+		boolean isDone = false;
+
+		while (!isDone) {
+
+			// get InetAddress 
+			InetAddress address = InetAddress.getByName("localhost");
+
+			Scanner scanner = new Scanner(System.in);
+
+			System.out.println("Enter your message: ");
+			String strMessage = scanner.nextLine();
+
+			// convert the message to bytes.
+			byte[] message = new String(strMessage).getBytes();
+			
+			// create a packet with destination port and address.
+			DatagramPacket sendPacket = new DatagramPacket(message, message.length, address, UDP_PORT);
+	
+			// send the newly created packet.
+			clientSocket.send(sendPacket);
+
+			// create a buffer for a new incoming packet.
+			byte[] newMessage = new byte[1024];
+
+			// Receive packet from the server.
+			DatagramPacket receivePacket = new DatagramPacket(newMessage, newMessage.length);
+
+			// receive the payload of the packet.
+			clientSocket.receive(receivePacket);
+
+			// turn the bytes into a string.
+			String newStr = new String(receivePacket.getData());
+			
+			System.out.println(newStr);
+		}
+	}
+}
---
+++ 0033/server/Main.java
@@ -0,0 +1,93 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.LinkedList;
+
+public class Main {
+
+  // server runs on this port.
+  private static int UDP_POR = 9876;
+
+  // holds the users currently logged in.
+  private static LinkedList<String> users = new LinkedList<>();
+
+  // hold list of users addresses currently logged into the server.
+  private static LinkedList<InetAddress> users_addresses = new LinkedList<>();
+
+  // hold users corresponding ports.
+  private static LinkedList<Integer> users_ports = new LinkedList<>();
+
+  /** This method takes a socket, users, users_addresses and users_ports.
+   * 
+   *  It then creates a packet and sends it to all the users currently online.
+   * 
+   */
+  private static void handleRequests(DatagramSocket socket, LinkedList<String> users, LinkedList<InetAddress> addresses, LinkedList<Integer> ports, String message) throws Exception {
+    
+    String[] req = message.split(" ");
+
+    String command = req[0];
+
+    if (command.equalsIgnoreCase("HELO")) {
+
+      String user = req[1];
+      String response = "HELO " + user;
+
+      byte[] newMessage = new String(response).getBytes();
+
+      for (int x = 0; x < users.size(); x++) {
+        DatagramPacket sendPacket = new DatagramPacket(newMessage, newMessage.length, addresses.get(x), ports.get(x));
+      
+        socket.send(sendPacket);
+      }
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+
+    // Creste a server socket.
+    DatagramSocket serverSocket = new DatagramSocket(UDP_POR);
+    
+    System.out.println("Server running on port " + UDP_POR);
+
+    // loop forever.
+    for(;;) {
+  
+      // create a bufferer to hold incoming messages.
+      byte[] message = new byte[1024];
+  
+      // Receive the packet.
+      DatagramPacket receivePacket = new DatagramPacket(message, message.length);
+      serverSocket.receive(receivePacket);
+
+      System.out.println("Packet information: ");
+
+      System.out.println("Address: " + receivePacket.getAddress());
+      
+      // add address of user to addresses.
+      users_addresses.add(receivePacket.getAddress());
+
+      System.out.println("Port: " + receivePacket.getPort());
+      
+      // add port no of user to user_ports
+      users_ports.add(receivePacket.getPort());
+
+      System.out.println('\n');
+
+      String receiveStr = new String(receivePacket.getData());
+
+      String[] incoming = receiveStr.split(" ");
+
+      if (incoming[0].equalsIgnoreCase("HELO")) {
+
+        users.add(incoming[1]);
+        
+        handleRequests(serverSocket, users, users_addresses, users_ports, receiveStr);
+      }
+  
+      System.out.println("Received message: ");
+      System.out.println(receiveStr);
+      System.out.println('\n');
+    }
+  }
+}
