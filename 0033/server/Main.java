98a9e6edaaa351b854be693032972b810518f0af
Kananelo Maxwell <kananelom.maxwell4@gmail.com>
2019-09-09 14:08:14 +0200

    Update Main.java

--- 0033/server/Main.java
+++ 0033/server/Main.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 
 public class Main {
 
+  private static DatagramSocket serverSocket;
+
   // server runs on this port.
   private static int UDP_POR = 9876;
 
@@ -17,60 +19,144 @@ public class Main {
   // hold users corresponding ports.
   private static LinkedList<Integer> users_ports = new LinkedList<>();
 
-  /**
-   * This method takes a socket, users, users_addresses and users_ports.
+  /** This method sends response to a user.
    * 
-   * It then creates a packet and sends it to all the users currently online.
-   * 
+   * @param response
+   * @param user
+   * @param packet
+   * @throws Exception
    */
-  private static void handleRequests(DatagramSocket socket, LinkedList<String> users, LinkedList<InetAddress> addresses,
-      LinkedList<Integer> ports, String message) throws Exception {
+  private static void respondToClient(String response, String user, DatagramPacket packet) throws Exception {
+    DatagramPacket response_packet = new DatagramPacket(new String(response).getBytes(),  response.length(), packet.getAddress(), packet.getPort());
+    serverSocket.send(response_packet);
+  }
 
-    String[] req = message.split(" ");
 
-    String command = req[0];
+  /** This function prints out active users along
+   *  with their computer details.
+   * 
+   * @param packet
+   */
+  public static void displayActiveUsers(DatagramPacket packet) throws Exception {
+    
+    // send each clients information as a packet.
+    for (int x = 0; x < users.size(); x++) {
 
-    if (command.equalsIgnoreCase("HELO")) {
+      // formulate a response to client.
+      String response = users.get(x) + " " + users_addresses.get(x) +  " " + users_ports.get(x);
 
-      // add client to online users.
-
-      String user = req[1];
-
-      String response = "HELO " + user + " " + users.indexOf(user);
-
-      System.out.println(response);
-
-      byte[] newMessage = new String(response).getBytes();
-
-      for (int x = 0; x < users.size(); x++) {
-        DatagramPacket sendPacket = new DatagramPacket(newMessage, newMessage.length, addresses.get(x), ports.get(x));
-
-        socket.send(sendPacket);
-      }
-    } else if (command.equalsIgnoreCase("BYE")) {
-      // remove client a list of active users.
-    } else if (command.equalsIgnoreCase("BUDDIES")) {
-      // send a client a list of active users.
-    } else if (command.equalsIgnoreCase("BROADCAST")) {
-      // send the message to all active users.
-    } else if (command.equalsIgnoreCase("YOBUDDY")) {
-      // send the message directly to user the user.
-        // get the user for list of active users.
-        // user the IP and PORT to send message directly.
-    } else {
-      // this is an invalid commmand.
-      // respond with an error message.
+      // create a packet and send it to a client.
+      DatagramPacket responsePacket = new DatagramPacket(new String(response).getBytes(), response.length(), packet.getAddress(), packet.getPort());
+      serverSocket.send(responsePacket);
     }
   }
 
+  public static void handleInitialConnection(String message, DatagramPacket packet) throws Exception {
+       
+        // get the name of the client
+        String client = message.split(" ")[1];
+
+        if (users.size() > 0) {
+          displayActiveUsers(packet);
+        }
+
+        // add user to active users.
+        addUser(client, packet);
+
+        System.out.println("C" + users.size() + " starts up");
+
+        // formulate a response.
+        String response = "AWE " + client + ", " + users.size();
+
+        System.out.println(response);
+
+        respondToClient(response, client, packet);
+  }
+
+  /** This function add a user to a list of active users.
+   * 
+   * @param name
+   * @param packet
+   */
+  public static void addUser(String name, DatagramPacket packet) {
+    users.add(name);
+    users_addresses.add(packet.getAddress());
+    users_ports.add(packet.getPort());
+  }
+
+  /** This function removes a users
+   *  from a list of active users.
+   * 
+   * @param packet
+   */
+  public static void removeUser(DatagramPacket packet) {
+    int port = packet.getPort();
+
+    int index = users_ports.indexOf(port);
+
+    // remove user information.
+    users.remove(index);
+    users_ports.remove(index);
+    users_addresses.remove(index);
+  }
+
+  /** This function broadcasts a message to all
+   *  active clients.
+   * 
+   * @param message
+   */
+  public static void broadcastToAllClient(String message) throws Exception {
+
+    String[] responseArr = message.split(" ");
+    String response = "";
+
+    // extract message from a client command.
+    for (int x = 1; x < responseArr.length; x++) {
+      response += responseArr[x] + ' ';
+    }
+
+    response = response.trim();
+
+    // send the response to all acive clients.
+    for (int x = 0; x < users.size(); x++) {
+
+      // create a datagram packet and send it.
+      DatagramPacket responsePacket = new DatagramPacket(new String(response).getBytes(), response.length(), users_addresses.get(x), (int)users_ports.get(x));
+      serverSocket.send(responsePacket);
+    }
+  }
+
+  /** This function respond with an error to the user.
+   * 
+   * @param packet
+   */
+  public static void sendError(DatagramPacket packet) throws Exception {
+
+    String response = new String("OUCH");
+
+    // formulate a response packet to the client for the error they made.
+    DatagramPacket datagramPacket = new DatagramPacket(response.getBytes(), response.length(), packet.getAddress(), packet.getPort());
+    serverSocket.send(datagramPacket);
+  }
+
+  /** This is the main method.
+   * 
+   * @param args
+   * @throws Exception
+   */
   public static void main(String[] args) throws Exception {
 
     // Creste a server socket.
-    DatagramSocket serverSocket = new DatagramSocket(UDP_POR);
+    serverSocket = new DatagramSocket(UDP_POR);
 
-    System.out.println("Server running on port " + UDP_POR);
+    System.out.println("S - indicates server.");
+    System.out.println("Ca - indicates client A.");
+    System.out.println("Cb - indicates client B.");
 
-    // loop forever.
+    System.out.println();
+    System.out.println("Server is running");
+
+    // loop forever and wait for incoming connection.
     for (;;) {
 
       // create a bufferer to hold incoming messages.
@@ -80,43 +166,33 @@ public class Main {
       DatagramPacket receivePacket = new DatagramPacket(message, message.length);
       serverSocket.receive(receivePacket);
 
-      System.out.println("Packet information: ");
-
-      System.out.println("Address: " + receivePacket.getAddress());
-
-      // add address of user to addresses.
-      users_addresses.add(receivePacket.getAddress());
-
-      System.out.println("Port: " + receivePacket.getPort());
-
-      // add port no of user to user_ports
-      users_ports.add(receivePacket.getPort());
-
-      System.out.println('\n');
-
       String receiveStr = new String(receivePacket.getData());
 
-      String[] incoming = receiveStr.split(" ");
+      if (receiveStr.startsWith("HELO")) {
 
-      if (incoming[0].equalsIgnoreCase("HELO")) {
+        handleInitialConnection(receiveStr, receivePacket);
+      } else if (receiveStr.startsWith("BROADCAST")) {
 
-        String user = incoming[0];
+        broadcastToAllClient(receiveStr);
 
-        users.add(user);
-
-        handleRequests(serverSocket, users, users_addresses, users_ports, receiveStr);
-
-      } else if (incoming[0].equalsIgnoreCase("BROADCAST")) {
         System.out.println("Server is message to all clients.");
-      } else if (incoming[0].equalsIgnoreCase("BUDDIES")) {
-        System.out.println("Server is displaying all users currently logged in.");
-      } else if (incoming[0].equalsIgnoreCase("YOBUDDY")) {
-        System.out.println("Client is talking to another client.");
-      } else if (incoming[0].equalsIgnoreCase("BYE")) {
-        System.out.println("Client is logging out.");
-      }
+      } else if (receiveStr.startsWith("BUDDIES")) {
+        // display all active users.
+        displayActiveUsers(receivePacket);
 
-      System.out.println("\n");
+        System.out.println("Displaying all active users.");
+      } else if (receiveStr.startsWith("YOBUDDY")) {
+        System.out.println("Client is talking to another client.");
+      } else if (receiveStr.startsWith("BYE")) {
+        // remove client from active client.
+        removeUser(receivePacket);
+
+        System.out.println("Removing client from active clients.");
+      } else {
+        sendError(receivePacket);
+
+        System.out.println("Some error happened.");
+      }
     }
   }
 }
