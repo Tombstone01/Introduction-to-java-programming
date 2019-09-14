7167c3ae5af9b0b833aae0f39fda9a1b9873770c
Kananelo Maxwell <kananelom.maxwell4@gmail.com>
2019-09-14 12:41:53 +0200

    Server can communicate

--- 0040/server/BUKAHandler.java
+++ 0040/server/BUKAHandler.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -16,89 +17,157 @@ public class BUKAHandler implements Runnable {
 	private Socket clientSocket = null;
 
 	private BufferedReader br = null;
-	private BufferedWriter bw = null;
+	private PrintWriter bw = null;
 
 	private String usersFile = "data/users.txt";
-	private String pdfFile = "data/PdfList.txt";
+  private String pdfFile = "data/PdfList.txt";
+
+  private static LinkedList<String> activeUsers = null;
+  private static LinkedList<Integer> user_ports = null;
 
 	public BUKAHandler(Socket newConnectionToClient) {
 
-		this.clientSocket = newConnectionToClient;
+    this.activeUsers = new LinkedList<>();
+    this.user_ports = new LinkedList<>();
 
-		// set up stream of client connection.
-		this.br = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
-		this.bw = new BufferedWriter(this.clientSocket.getOutputStream(), true);
+    try {
+      this.clientSocket = newConnectionToClient;
+      this.br = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+      this.bw = new PrintWriter(this.clientSocket.getOutputStream(), true);
+	  } catch (Exception e) {
+        e.printStackTrace();
+	  }
 	}
 
 	public void run() {
-		// Process commands from client
-		String req = br.readLine();
 
-		StringTokenizer tokens = new StringTokenizer(req);
+    for (;;) {
+      String req = null;
 
-		String command = tokens.nextToken();
+      // Process commands from client
+      try {
+        req = br.readLine();	
+      } catch (Exception e) {
+        System.out.println("There is a problem reading data from client.");
+      }
+  
+      StringTokenizer tokens = new StringTokenizer(req);
+  
+      String command = tokens.nextToken();
+      
+      System.out.println("Next command: " + command);
+  
+      switch (command) {
+      case "AUTH":
+  
+        // get username and password
+        String username = tokens.nextToken();
+        String password = tokens.nextToken();
+  
+        // check if user exists.
+        boolean isFound = this.matchUser(username, password);
+  
+        // if the user is found.
+        if (isFound) {
+  
+          this.activeUsers.add(username);
+          this.user_ports.add(this.clientSocket.getPort());
+  
+          // formulate response message and send message
+          System.out.println("Login successful.");
+  
+          String message = "Login sucess";
+          this.sendResponse(message);
+        }
+  
+        break;
+      case "LIST":
+  
+        ArrayList<String> users = this.getFileList();
+  
+        String message = "";
+  
+        for (int x = 0; x < users.size(); x++) {
+          message += users.get(x);
+        }
+  
+        sendResponse(message);
+  
+        break;
+      case "PDFRT":
+  
+        String pdf_id = tokens.nextToken();
+        String file = this.idToFile(pdf_id);
+  
+        sendResponse(file);
+  
+        break;
+      case "LOGOUT":
+  
+         System.out.println("Client trying to logout.");
+  
+        int port = this.clientSocket.getPort();
+  
+        if (this.user_ports.indexOf(port) != -1) {
 
-		switch (command) {
-		case "AUTH":
+          System.out.println("Removing user ... ");
 
-			// get username and password
-			String username = tokens.nextToken();
-			String password = tokens.nextToken();
+          int index = this.user_ports.indexOf(port);
+  
+          this.user_ports.remove(user_ports.get(index));
 
-			// check if user exists.
-			bool isFound = this.matchUser(username, password);
+          sendResponse("User loggout success.");
 
-			// if the user is found.
-			if (isFound) {
-
-				// formulate response message and send message
-				String message = "Login sucess";
-				this.sendResponse(message);
-			}
-
-			break;
-		case "LIST":
-
-			ArrayList<String> users = this.getFileList();
-
-			String message = "";
-
-			for (int x = 0; x < users.size(); x++) {
-				message += users.get(x);
-			}
-
-			sendResponse(message);
-
-			break;
-		case "PDFRT":
-
-			String pdf_id = tokens.nextToken();
-			String file = this.idToFile(pdf_id);
-
-			sendResponse(file);
-
-			break;
-		case "LOGOUT":
-			break;
-		default:
-			break;
-		}
+        } else {
+          sendError("You must be logged in to logout.");
+        }
+        
+        break;
+      default:
+        break;
+      }
+    }
 	}
 
+  /** This function takes username and password
+   *  Then returns a true of false depending 
+   *  on whether or not, a user exists in text file.
+   * 
+   * @param username
+   * @param password
+   * @return
+   */
 	private boolean matchUser(String username, String password) {
 
+    // indicate whether a client exists or not
 		boolean found = false;
 
-		File userFile = new File(new File(this.usersFile));
+    // File contains user credentials
+		File userFile = new File(this.usersFile);
 
 		try {
-			Scanner scan = new Scanner(userFile);
+      // Open file
+      Scanner scan = new Scanner(userFile);
+      
+      // while there is a newline in file
 			while (scan.hasNextLine() && !found) {
-				String line = scan.nextLine();
+
+        // reads line from file
+        String line = scan.nextLine();
+        
+        // split the line by space
 				String lineSec[] = line.split("\\s");
 
-				// Compare user
+        // get username from file
+        String user = lineSec[0];
+        
+        // get password from file
+				String pass = lineSec[1];
 
+        // if user exists in database
+				if (user.equalsIgnoreCase(username) && pass.equalsIgnoreCase(password)) {
+				    found = true;
+				}
 			}
 			scan.close();
 		} catch (IOException ex) {
@@ -130,7 +199,7 @@ public class BUKAHandler implements Runnable {
 
 	private String idToFile(String ID) {
 		String result = "";
-		File lstFile = new File(/* File location */);
+		File lstFile = new File(pdfFile);
 		try {
 			Scanner scan = new Scanner(lstFile);
 
@@ -159,6 +228,13 @@ public class BUKAHandler implements Runnable {
 	private void sendResponse(String message) {
 		String response = "200 " + message;
 
-		bw.write(response);
-	}
+    bw.println(response);
+    bw.flush();
+  }
+  
+  private void sendError(String message) {
+    String response = "500 " + message;
+    
+    bw.println(response);
+  }
 }
